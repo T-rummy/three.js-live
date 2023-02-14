dc2b5f8736da8cfae270180453fe5eb7ebb56659
Ruminert <ruminertt@Tanners-MacBook-Air.local>
2023-02-14 17:44:35 -0600

    .

--- src/script.js
+++ src/script.js
@@ -36,7 +36,7 @@ fontLoader.load(
 
         // Text
         const textGeometry = new TextGeometry(
-            'Hello Maddie',
+            'Hello BITCH',
             {
                 font: font,
                 size: 0.5,
