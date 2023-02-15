7f8bb5e89a897c08127dcbc74b2594df4dc86afd
Ruminert <ruminertt@Tanners-MacBook-Air.local>
2023-02-14 18:08:12 -0600

    changed perspective

--- src/script.js
+++ src/script.js
@@ -100,7 +100,7 @@ window.addEventListener('resize', () =>
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+const camera = new THREE.PerspectiveCamera(125, sizes.width / sizes.height, 0.1, 100)
 camera.position.x = 1
 camera.position.y = 1
 camera.position.z = 2
